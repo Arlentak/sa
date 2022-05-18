f0c8ac24cc762764ac79342eddf8b55d074ab036
aaa <103915651+Arlentak@users.noreply.github.com>
2022-05-19 03:01:44 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,34 @@
 terraform {
+  required_version = ">= 0.13"
+
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.16.0"
+    docker-utils = {
+      source = "Kaginari/docker-utils"
     }
   }
 }
+variable "docker_tcp_host" {}
+variable "docker_container_name" {}
 
-
-provider "docker" {
+provider "docker-utils" {
+    host = var.docker_tcp_host
 }
-# Pulls the image
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
-}
+resource "docker-utils_exec" "exec" {
 
-# Create a container
-resource "docker_container" "foo" {
-  image = docker_image.ubuntu.latest
-  name  = "foo"
+  container_name = var.docker_container_name    #(Required) the container alias or id
+
+  # Exec Options
+
+  attach_stderr = true    # optional default false
+  attach_stdin = true     # optional default false
+  attach_stdout = true    # optional default false
+  detach  = true          # optional default false
+  tty  = true             # optional default false
+  privileged  = false     # optional default false
+  user  = "root"          # optional default root
+  working_dir = "/home"   # optional default root folder
+
+  # Exec commands
+  commands = ["/bin/bash","-c","ls"]                    # (Required) commands will be applied on apply
+                                                 # (Optional) environment will be applied on destroy
 }
